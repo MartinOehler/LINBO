6570e2149e9aeb86c7027132af555d9a5be6ce4e
Klaus Knopper <info@knopper.net>
2010-10-16 23:06:08 +0000

    - Delete old kernel

--- Kernel/linux-2.6.33/include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 9 */
-/* SMP */
-#define UTS_MACHINE "i386"
-#define UTS_VERSION "#9 SMP Sat May 15 10:42:17 CEST 2010"
-#define LINUX_COMPILE_TIME "10:42:17"
-#define LINUX_COMPILE_BY "knopper"
-#define LINUX_COMPILE_HOST "eeepc"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.4.3 (Debian 4.4.3-1) "
